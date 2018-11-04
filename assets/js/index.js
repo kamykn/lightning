9372e9f9485c43d3acbd2b891da34936dd27ec2d
kamykn <to.kamykn@gmail.com>
2018-11-04 16:06:02 +0900

    タブ切り替えの準備

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,104 +13,122 @@ window.onload = function() {
 }
 
 function init() {
-	// setHistoryTab()
-	// setBookmarksTab()
-	setTabsTab()
 	muff.setReturnListLength(20)
-
-	vueInit();
-}
-
-function setTabsTab() {
-	chrome.tabs.query({currentWindow: true}, function(tabs) {
-		let searchWordList = []
-
-		tabs.forEach(function(tab) {
-			searchWordList.push({
-				// path: bookmark.index,
-				title: tab.title,
-				url: tab.url
-			})
-		});
-
-		muff.setSearchWordList(searchWordList)
-	});
-
-	// メモ
-	// すでにあるタブを開くには
-	// chrome.tabs.update(tabs[i].id, {active: true});
-	// https://stackoverflow.com/questions/36000099/check-if-window-is-already-open-from-a-non-parent-window-chrome-extension
-}
-
-function setBookmarksTab() {
-	chrome.bookmarks.getTree((bookmarksTree) => {
-		let searchWordList = []
-		searchWordList = pushBookmarkListRecursive(bookmarksTree, searchWordList)
-		console.log(searchWordList)
-		muff.setSearchWordList(searchWordList)
-	});
-}
-
-function pushBookmarkListRecursive(bookmarksTree, searchWordList) {
-	for (let i =0; i < bookmarksTree.length; i++) {
-		let bookmark = bookmarksTree[i];
-
-		if (bookmark.url) {
-			searchWordList.push({
-				// path: bookmark.index,
-				title: bookmark.title,
-				url: bookmark.url
-			})
-		}
-
-		if (bookmark.children) {
-			searchWordList = pushBookmarkListRecursive(bookmark.children, searchWordList);
-		}
-	}
-
-	return searchWordList
-}
-
-function setHistoryTab() {
-	// 1年分
-	const startTime = new Date().getTime() - (1000 * 60 * 60 * 24 * 265)
-	const query = {
-		text: '',
-		startTime: startTime,
-		maxResults: 50000
-	}
-
-	let historyList = []
-	chrome.history.search(query, function (results) {
-		// resultsは配列なので、forEach()関数を実行することが出来る
-		const reverseResult = results.reverse()
-		reverseResult.forEach(function (result) {
-			// resultひとつひとつがHistoryItem形式
-			historyList.push({
-				url: result.url,
-				title: result.title
-			})
-		});
-
-		muff.setSearchWordList(historyList)
-	})
+	vueInit()
 }
 
 function vueInit() {
-	new Vue({
+	let vm = new Vue({
 		el: '#muffin',
 		data: {
-			inputValue: ''
+			inputValue: '',
+			currentSearchType: null,
+			searchTypes: {
+				history:   1,
+				bookmarks: 2,
+				tabs:      3
+			}
 		},
 		methods: {
+			init() {
+				this.currentSearchType = this.searchTypes.history;
+				// this.setHistoryTab()
+				this.setBookmarksTab()
+				// this.setTabsTab()
+			},
 			search(inputValue) {
 				return new Promise(resolve => {
 					let results = muff.search(inputValue)
 					resolve(results)
 				})
 			},
-			setData(results) {
+			setSearchResultsToData(results) {
 				return results.list
+			},
+			setHistoryTab() {
+				this.currentSearchType = this.searchTypes.history
+
+				// 1年分
+				const startTime = new Date().getTime() - (1000 * 60 * 60 * 24 * 265)
+				const query = {
+					text: '',
+					startTime: startTime,
+					maxResults: 50000
+				}
+
+				let historyList = []
+				chrome.history.search(query, function (results) {
+					const reverseResult = results.reverse()
+					reverseResult.forEach(function (result) {
+						// resultひとつひとつがHistoryItem形式
+						historyList.push({
+							url: result.url,
+							title: result.title
+						})
+					});
+
+					muff.setSearchWordList(historyList)
+				})
+			},
+			setTabsTab() {
+				this.currentSearchType = this.searchTypes.tabs
+
+				chrome.tabs.query({currentWindow: true}, function(tabs) {
+					console.log(tabs)
+					let searchWordList = []
+
+					tabs.forEach(function(tab, index) {
+						searchWordList.push({
+							index: tab.index.toString(),
+							title: tab.title,
+							url: tab.url
+						})
+					});
+
+					console.log(searchWordList)
+					muff.setSearchWordList(searchWordList)
+				});
+
+				// メモ
+				// すでにあるタブを開くには
+				// chrome.tabs.update(tabs[i].id, {active: true});
+				// https://stackoverflow.com/questions/36000099/check-if-window-is-already-open-from-a-non-parent-window-chrome-extension
+			},
+			setBookmarksTab() {
+				this.currentSearchType = this.searchTypes.bookmarks
+
+				chrome.bookmarks.getTree((bookmarksTree) => {
+					let searchWordList = []
+					searchWordList = this.pushBookmarkListRecursive(bookmarksTree, searchWordList)
+					muff.setSearchWordList(searchWordList)
+				});
+			},
+			pushBookmarkListRecursive(bookmarksTree, searchWordList, parentPath) {
+				console.log(parentPath)
+				if (typeof parentPath == 'undefined') {
+					parentPath = ''
+				}
+
+				for (let i =0; i < bookmarksTree.length; i++) {
+					let bookmark = bookmarksTree[i];
+
+					if (bookmark.url) {
+						searchWordList.push({
+							parentPath: parentPath, // TODO: 検索ignoreしたい
+							path: parentPath + bookmark.title,
+							title: bookmark.title,
+							url: bookmark.url
+						})
+					}
+
+					if (bookmark.children) {
+
+						let currentPath = parentPath + bookmark.title + '/'
+						searchWordList = this.pushBookmarkListRecursive(bookmark.children, searchWordList, currentPath);
+					}
+				}
+
+				return searchWordList
 			}
 		},
 		subscriptions() {
@@ -119,9 +137,11 @@ function vueInit() {
 					pluck('newValue'),
 					debounceTime(500),
 					switchMap(this.search),
-					map(this.setData)
+					map(this.setSearchResultsToData)
 				)
 			}
 		}
 	})
+
+	vm.init()
 }
--- dist/0.bundle.js
+++ dist/0.bundle.js
@@ -29,104 +29,126 @@ window.onload = function () {
 };
 
 function init() {
-  // setHistoryTab()
-  // setBookmarksTab()
-  setTabsTab();
   muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setReturnListLength(20);
   vueInit();
 }
 
-function setTabsTab() {
-  chrome.tabs.query({
-    currentWindow: true
-  }, function (tabs) {
-    var searchWordList = [];
-    tabs.forEach(function (tab) {
-      searchWordList.push({
-        // path: bookmark.index,
-        title: tab.title,
-        url: tab.url
-      });
-    });
-    muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(searchWordList);
-  });
-}
-
-function setBookmarksTab() {
-  chrome.bookmarks.getTree(function (bookmarksTree) {
-    var searchWordList = [];
-    searchWordList = pushBookmarkListRecursive(bookmarksTree, searchWordList);
-    console.log(searchWordList);
-    muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(searchWordList);
-  });
-}
-
-function pushBookmarkListRecursive(bookmarksTree, searchWordList) {
-  for (var i = 0; i < bookmarksTree.length; i++) {
-    var bookmark = bookmarksTree[i];
-
-    if (bookmark.url) {
-      searchWordList.push({
-        // path: bookmark.index,
-        title: bookmark.title,
-        url: bookmark.url
-      });
-    }
-
-    if (bookmark.children) {
-      searchWordList = pushBookmarkListRecursive(bookmark.children, searchWordList);
-    }
-  }
-
-  return searchWordList;
-}
-
-function setHistoryTab() {
-  // 1年分
-  var startTime = new Date().getTime() - 1000 * 60 * 60 * 24 * 265;
-  var query = {
-    text: '',
-    startTime: startTime,
-    maxResults: 50000
-  };
-  var historyList = [];
-  chrome.history.search(query, function (results) {
-    // resultsは配列なので、forEach()関数を実行することが出来る
-    var reverseResult = results.reverse();
-    reverseResult.forEach(function (result) {
-      // resultひとつひとつがHistoryItem形式
-      historyList.push({
-        url: result.url,
-        title: result.title
-      });
-    });
-    muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(historyList);
-  });
-}
-
 function vueInit() {
-  new vue_dist_vue_esm_js__WEBPACK_IMPORTED_MODULE_1__["default"]({
+  var vm = new vue_dist_vue_esm_js__WEBPACK_IMPORTED_MODULE_1__["default"]({
     el: '#muffin',
     data: {
-      inputValue: ''
+      inputValue: '',
+      currentSearchType: null,
+      searchTypes: {
+        history: 1,
+        bookmarks: 2,
+        tabs: 3
+      }
     },
     methods: {
+      init: function init() {
+        this.currentSearchType = this.searchTypes.history; // this.setHistoryTab()
+
+        this.setBookmarksTab(); // this.setTabsTab()
+      },
       search: function search(inputValue) {
         return new Promise(function (resolve) {
           var results = muff__WEBPACK_IMPORTED_MODULE_0__["muff"].search(inputValue);
           resolve(results);
         });
       },
-      setData: function setData(results) {
+      setSearchResultsToData: function setSearchResultsToData(results) {
         return results.list;
+      },
+      setHistoryTab: function setHistoryTab() {
+        this.currentSearchType = this.searchTypes.history; // 1年分
+
+        var startTime = new Date().getTime() - 1000 * 60 * 60 * 24 * 265;
+        var query = {
+          text: '',
+          startTime: startTime,
+          maxResults: 50000
+        };
+        var historyList = [];
+        chrome.history.search(query, function (results) {
+          var reverseResult = results.reverse();
+          reverseResult.forEach(function (result) {
+            // resultひとつひとつがHistoryItem形式
+            historyList.push({
+              url: result.url,
+              title: result.title
+            });
+          });
+          muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(historyList);
+        });
+      },
+      setTabsTab: function setTabsTab() {
+        this.currentSearchType = this.searchTypes.tabs;
+        chrome.tabs.query({
+          currentWindow: true
+        }, function (tabs) {
+          console.log(tabs);
+          var searchWordList = [];
+          tabs.forEach(function (tab, index) {
+            searchWordList.push({
+              index: tab.index.toString(),
+              title: tab.title,
+              url: tab.url
+            });
+          });
+          console.log(searchWordList);
+          muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(searchWordList);
+        }); // メモ
+        // すでにあるタブを開くには
+        // chrome.tabs.update(tabs[i].id, {active: true});
+        // https://stackoverflow.com/questions/36000099/check-if-window-is-already-open-from-a-non-parent-window-chrome-extension
+      },
+      setBookmarksTab: function setBookmarksTab() {
+        var _this = this;
+
+        this.currentSearchType = this.searchTypes.bookmarks;
+        chrome.bookmarks.getTree(function (bookmarksTree) {
+          var searchWordList = [];
+          searchWordList = _this.pushBookmarkListRecursive(bookmarksTree, searchWordList);
+          muff__WEBPACK_IMPORTED_MODULE_0__["muff"].setSearchWordList(searchWordList);
+        });
+      },
+      pushBookmarkListRecursive: function pushBookmarkListRecursive(bookmarksTree, searchWordList, parentPath) {
+        console.log(parentPath);
+
+        if (typeof parentPath == 'undefined') {
+          parentPath = '';
+        }
+
+        for (var i = 0; i < bookmarksTree.length; i++) {
+          var bookmark = bookmarksTree[i];
+
+          if (bookmark.url) {
+            searchWordList.push({
+              parentPath: parentPath,
+              // TODO: 検索ignoreしたい
+              path: parentPath + bookmark.title,
+              title: bookmark.title,
+              url: bookmark.url
+            });
+          }
+
+          if (bookmark.children) {
+            var currentPath = parentPath + bookmark.title + '/';
+            searchWordList = this.pushBookmarkListRecursive(bookmark.children, searchWordList, currentPath);
+          }
+        }
+
+        return searchWordList;
       }
     },
     subscriptions: function subscriptions() {
       return {
-        results: this.$watchAsObservable('inputValue').pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["pluck"])('newValue'), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["debounceTime"])(500), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["switchMap"])(this.search), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["map"])(this.setData))
+        results: this.$watchAsObservable('inputValue').pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["pluck"])('newValue'), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["debounceTime"])(500), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["switchMap"])(this.search), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_4__["map"])(this.setSearchResultsToData))
       };
     }
   });
+  vm.init();
 }
 
 /***/ }),
